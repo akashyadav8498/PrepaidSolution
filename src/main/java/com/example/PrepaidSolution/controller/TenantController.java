eab48bcd38814972bf5b83ac4d58687c9299215e
utkarsh <utkarshtyagi1612@gmail.com>
2026-04-14 14:57:08 +0530

    some files deleted

--- src/main/java/com/example/PrepaidSolution/controller/TenantController.java
+++
@@ -1,4 +0,0 @@
-package com.example.PrepaidSolution.controller;
-
-public class TenantController {
-}
--- src/main/java/com/example/PrepaidSolution/service/TenantService.java
+++
@@ -1,4 +0,0 @@
-package com.example.PrepaidSolution.service;
-
-public class TenantService {
-}
